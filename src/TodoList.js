f184ea6686b91e5f7161fcabaa21003cfa8d6db7
Rogerd Júnior Ribeiro Bitarães <you@example.com>
2019-01-15 16:32:36 -0200

    insert/delete todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,97 @@
-import React from "react";
-
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput
+} from "react-native";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as TodoActions from "./store/actions/todos";
 
-const TodoList = ({ todos }) => (
-  <View>
-    {todos.map(todo => (
-      <Text>{todo}</Text>
-    ))}
-  </View>
-);
+class TodoList extends Component {
+  state = {
+    input: ""
+  };
+  render() {
+    const { input } = this.state;
+    const { todos, addTodo, removeTodo } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <TextInput
+              placeholder="Nova tarefa"
+              underlineColorAndroid="transparent"
+              onChangeText={input => this.setState({ input })}
+              value={input}
+              style={styles.input}
+            />
+            <TouchableOpacity
+              onPress={() => addTodo(input)}
+              style={styles.addTodo}
+            >
+              <Text style={styles.txtAdd}>ADICIONAR</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {todos.map(todo => (
+          <View key={todo.id} style={styles.todo}>
+            <Text>{todo.text}</Text>
+
+            <TouchableOpacity onPress={() => removeTodo(todo.id)}>
+              <Text>Excluir</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   todos: state.todos
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodoActions, dispatch);
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5
+  },
+  todo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 7,
+    padding: 3,
+    backgroundColor: "#999",
+    borderRadius: 5,
+    marginTop: 5
+  },
+  addTodo: {
+    padding: 8,
+    backgroundColor: "#339",
+    borderRadius: 7
+  },
+  txtAdd: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold"
+  },
+  input: {
+    borderRadius: 5,
+    backgroundColor: "#fff"
+  },
+  form: {
+    marginHorizontal: 5
+  }
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,9 +1,11 @@
-const INITIAL_STATE = ["fazer café", "estudar react native"];
+const INITIAL_STATE = [];
 
 export default function todos(state = INITIAL_STATE, action) {
   switch (action.type) {
     case "ADD_TODO":
-      return [...state, action.text];
+      return [...state, { id: Math.random(), text: action.payload.text }];
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.payload.id);
     default:
       return state;
   }
